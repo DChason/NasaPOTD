9ced4c78ddc6cace3f01be2b5acee341c0ae1c36
Damien Chason <damien@damienchason.com>
2019-06-19 23:10:25 -0400

    NasaPOTD - Initial commit

---
+++ nasacall.js
@@ -0,0 +1,24 @@
+var url = "https://api.nasa.gov/planetary/apod?api_key={YOUR_API_KEY}"
+
+$.ajax({
+  url: url,
+  success: function(results){
+    if("copyright" in results) {
+      $("#copyright").text("Image Credits: " + results.copyright);
+    } else {
+      $("#copyright").text("Image Credits: " + "Public Domain");
+      }
+
+    if(results.media_type == "video") {
+      $("#image").css("display", "none");
+      $("#vid").attr("src", results.url);
+    } else {
+      $("#vid").css("display", "none");
+      $("#image").attr("src", results.url);
+      }
+
+    $("#explanation").text(results.explanation);
+    $("#title").text(results.title);
+    $("#imageHD").attr("src", results.hdurl);
+  }
+});
