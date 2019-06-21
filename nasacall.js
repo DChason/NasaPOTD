1e1b156820b58ba1b0ab8266097513281c4cf9c2
Damien Chason <damien@damienchason.com>
2019-06-21 00:08:55 -0400

    NasaPOTD - Cleaning up a bit

--- nasacall.js
+++ nasacall.js
@@ -1,21 +1,21 @@
-var url = "https://api.nasa.gov/planetary/apod?api_key={YOUR_API_KEY}"
+var url = "https://api.nasa.gov/planetary/apod?api_key={YOUR_API_KEY}";
 
 $.ajax({
   url: url,
-  success: function(results){
-    if("copyright" in results) {
+  success: function(results) {
+    if ("copyright" in results) {
       $("#copyright").text("Image Credits: " + results.copyright);
     } else {
       $("#copyright").text("Image Credits: " + "Public Domain");
-      }
+    }
 
-    if(results.media_type == "video") {
+    if (results.media_type == "video") {
       $("#image").css("display", "none");
       $("#vid").attr("src", results.url);
     } else {
       $("#vid").css("display", "none");
       $("#image").attr("src", results.url);
-      }
+    }
 
     $("#explanation").text(results.explanation);
     $("#title").text(results.title);
